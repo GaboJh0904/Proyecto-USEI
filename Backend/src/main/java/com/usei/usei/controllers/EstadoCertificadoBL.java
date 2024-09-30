9c5e86e044f1d578effc5ea2299fbfb6bfdeb75b
GaboJh0904 <126207264+GaboJh0904@users.noreply.github.com>
2024-09-30 15:29:12 -0400

    Correccion error de Service en Estado Certificado BL

--- Backend/src/main/java/com/usei/usei/controllers/EstadoCertificadoBL.java
+++ Backend/src/main/java/com/usei/usei/controllers/EstadoCertificadoBL.java
@@ -3,6 +3,7 @@ package com.usei.usei.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.usei.usei.models.Certificado;
@@ -10,6 +11,7 @@ import com.usei.usei.models.EstadoCertificado;
 import com.usei.usei.models.Estudiante;
 import com.usei.usei.repositories.EstadoCertificadoDAO;
 
+@Service
 public class EstadoCertificadoBL implements EstadoCertificadoService{
     
     private final EstadoCertificadoDAO estadoCertificadoDAO;
