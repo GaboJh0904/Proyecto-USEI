ca31ee87569bd7d31dc41452a386f101c52c4fbb
Laaauuura <laura.mallea@ucb.edu.bo>
2024-10-21 16:05:29 -0400

    Paginación preguntas

--- Backend/src/main/java/com/usei/usei/repositories/PreguntaDAO.java
+++ Backend/src/main/java/com/usei/usei/repositories/PreguntaDAO.java
@@ -1,12 +1,17 @@
 package com.usei.usei.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.usei.usei.models.Pregunta;
 
 @Repository
-public interface PreguntaDAO extends  JpaRepository<Pregunta, Long> {
+public interface PreguntaDAO extends JpaRepository<Pregunta, Long> {
 
-    
+    // Método para obtener preguntas con filtrado
+    @Query("SELECT p FROM Pregunta p WHERE LOWER(p.pregunta) LIKE LOWER(CONCAT('%', :filter, '%'))")
+    Page<Pregunta> findByFilter(Pageable pageable, String filter);
 }
