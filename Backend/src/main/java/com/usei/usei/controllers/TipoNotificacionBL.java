d01c96f6efe158f9da27b80b978dfb19be597c98
Alvaro Gabriel Jurado Huarachi <alvaro.jurado@ucb.edu.bo>
2024-11-28 16:00:32 -0400

    Bug de Notificacion

--- Backend/src/main/java/com/usei/usei/controllers/TipoNotificacionBL.java
+++ Backend/src/main/java/com/usei/usei/controllers/TipoNotificacionBL.java
@@ -1,13 +1,13 @@
 package com.usei.usei.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.usei.usei.models.TipoNotificacion;
 import com.usei.usei.repositories.TipoNotificacionDAO;
-
-import java.util.Optional;
 
 @Service
 public class TipoNotificacionBL implements TipoNotificacionService {
@@ -42,15 +42,21 @@ public class TipoNotificacionBL implements TipoNotificacionService {
     @Override
     @Transactional
     public TipoNotificacion update(TipoNotificacion tipoNotificacion, Long id) {
-    Optional<TipoNotificacion> existingTipoNotificacion = tipoNotificacionDAO.findById(id);
-    if (existingTipoNotificacion.isPresent()) {
-        TipoNotificacion tipoNotificacionToUpdate = existingTipoNotificacion.get();
-        tipoNotificacionToUpdate.setTipo(tipoNotificacion.getTipo());
+        Optional<TipoNotificacion> existingTipoNotificacion = tipoNotificacionDAO.findById(id);
+        if (existingTipoNotificacion.isPresent()) {
+            TipoNotificacion tipoNotificacionToUpdate = existingTipoNotificacion.get();
+            tipoNotificacionToUpdate.setTipo(tipoNotificacion.getTipo());
 
-        return tipoNotificacionDAO.save(tipoNotificacionToUpdate);
-    } else {
-        throw new RuntimeException("Tipo Notificacion no encontrada con el id: " + id);
+            return tipoNotificacionDAO.save(tipoNotificacionToUpdate);
+        } else {
+            throw new RuntimeException("Tipo Notificacion no encontrada con el id: " + id);
+        }
     }
-}
+
+    @Override
+    @Transactional(readOnly = true)
+    public TipoNotificacion findByTipoNotificacion(String tipo) {
+        return tipoNotificacionDAO.findByTipo(tipo);
+    }
 
 }
